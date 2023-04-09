9e28f560d2495ca2862431e1890f4b5f2b57a6ba
torbik <liker1011@gmail.com>
2023-04-09 13:19:58 +0700

    migration is closed

--- Frontend-v1-Original/components/migration/migration.tsx
+++ Frontend-v1-Original/components/migration/migration.tsx
@@ -9,23 +9,23 @@ function Migration() {
     <div className="mt-32 flex h-full min-h-[calc(100vh-432px)] w-full flex-col items-center justify-evenly gap-8 xs:mt-32 min-[1201px]:mt-0">
       {v2 && (
         <div className="flex max-w-sm flex-col gap-2 rounded-lg border border-cantoGreen bg-black p-4 tracking-wide text-green-100 md:max-w-xl">
-          <div>Velocimeter v1 to v2 Migration.</div>
+          <div>Velocimeter v1 to v2 Migration is CLOSED.</div>
           <div>
-            Convert FLOW v1 to FLOW v2 1:1.
+            Migration is closed!
             <br />
             This is IRREVERSIBLE!
             <br />
-            In order to migrate liquidity from v1 to v2, you will need to
-            withdraw your v1 LPs, convert any v1 FLOW to v2, then deposit
-            liquidity via the v2{" "}
-            <Link href={"/liquidity"}>
-              <a className="font-medium text-cantoGreen underline hover:no-underline">
-                interface.
-              </a>
-            </Link>
+            Please use{" "}
+            <a
+              href="https://www.velocimeter.xyz/liquidity"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="font-medium text-cantoGreen hover:underline"
+            >
+              v2 app
+            </a>
+            .
           </div>
-          <div>MIGRATION NEEDS TO BE COMPLETED BEFORE THE NEXT EPOCH!</div>
-          <div className="">Go with the FLOW 🌊.</div>
         </div>
       )}
       <Paper
--- Frontend-v1-Original/components/ssWarning/ssWarning.tsx
+++ Frontend-v1-Original/components/ssWarning/ssWarning.tsx
@@ -9,35 +9,11 @@ export default function ffWarning({ close }) {
           Migration Warning
         </div>
         <div className="mb-3 text-base font-light text-lime-50">
-          Migration from v1 to v2 is LIVE.
+          Migration from v1 to v2 is CLOSED.
           <br />
-          The migration steps for everyone are straightforward:
+          The migration happened on 16th of March.
           <br />
-          1. veFLOW holders were airdropped new veFLOW.
-          <br />
-          2. FLOW_v1 holders & farmers should claim any remaining FLOW_v1
-          emissions and convert their tokens to FLOW_v2 via the MIGRATION tab on
-          the{" "}
-          <a
-            href="https://www.velocimeter.xyz/migration"
-            target="_blank"
-            rel="noreferrer noopener"
-            className="font-medium text-cantoGreen hover:underline"
-          >
-            v2 app
-          </a>
-          .
-          <br />
-          3. Liquidity providers should break their liquidity from the{" "}
-          <a
-            href="https://canto.velocimeter.xyz/liquidity"
-            target="_blank"
-            rel="noreferrer noopener"
-            className="font-medium text-cantoGreen hover:underline"
-          >
-            v1 app
-          </a>{" "}
-          and add it to the{" "}
+          Here is a link to{" "}
           <a
             href="https://www.velocimeter.xyz/liquidity"
             target="_blank"
@@ -49,27 +25,7 @@ export default function ffWarning({ close }) {
           .
         </div>
         <div className="text-base font-normal text-lime-50">
-          Pay Careful attention to the epoch timer!
-          <br />
-          <span className="font-semibold">
-            FLOW_v1 is only be redeemable for FLOW_v2 prior to the subsequent
-            epoch flip (00:00 UTC Thursday).
-          </span>
-          <br />
-          This is to avoid the V1 to continue to be farmed and converted. We
-          encourage those who want to add liquidity to FLOW pairs on v2 to do
-          so.
-          <br />
-          Shortly before epoch timer is up we will Kill all the gauges on v1
-          app.{" "}
-          <span className="font-semibold">
-            You MUST have your rewards claimed before this time.
-          </span>{" "}
-          You won&apos;t be able to claim rewards after the epoch flip on v1
-          app.
-          <br />
-          We will also empty the redeemer shorter after the epoch flip. So
-          migrate your v1 FLOW sooner than later.
+          This is a v1 app! This app is deprecated and will be shut down.
         </div>
       </div>
     </Dialog>
